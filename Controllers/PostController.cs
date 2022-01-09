e1b08a66ac790ec4b1ed3ffa2b052f62c053427f
Gryphon1999 <gslcpassed@gmail.com>
2022-01-09 15:48:34 +0545

    UpdateEdit

--- Controllers/PostController.cs
+++ Controllers/PostController.cs
@@ -22,23 +22,24 @@ namespace ManyToManyRelation.Controllers
 
         public IActionResult Index()
         {
-            var post = context.posts.Include(p=>p.PostCategories)
-            .ThenInclude(c=>c.Category)
-            .OrderByDescending(p=>p.Id);
+            var post = context.posts.Include(p => p.PostCategories)
+            .ThenInclude(c => c.Category)
+            .ToList();
             return View(post);
-        }        
+        }
 
         public IActionResult Create()
         {
-            ViewData["categories"]= context.categories.ToList();
+            ViewData["categories"] = context.categories.ToList();
             return View();
         }
 
         [HttpPost]
         public IActionResult Create(Post post, int[] SelectedCategoryIds)
         {
-            foreach(var item in SelectedCategoryIds){
-                post.PostCategories.Add(new PostCategory{CategoryId = item});
+            foreach (var item in SelectedCategoryIds)
+            {
+                post.PostCategories.Add(new PostCategory { CategoryId = item });
             }
             context.Add(post);
             context.SaveChanges();
@@ -47,36 +48,50 @@ namespace ManyToManyRelation.Controllers
         public IActionResult Edit(int id)
         {
             ViewData["categories"] = context.categories.ToList();
-            var post = context.posts.Include(p=>p.PostCategories).FirstOrDefault(x=>x.Id==id);
+            var post = context.posts.Include(p => p.PostCategories).FirstOrDefault(x => x.Id == id);
             return View(post);
         }
 
         [HttpPost]
-        public IActionResult Edit(Post post, int[] SelectedCategoryIds)
+        public IActionResult Edit(int id,Post post, int[] SelectedCategoryIds)
         {
+            var model = context.posts.Include(p=>p.PostCategories).FirstOrDefault(x=>x.Id==id);
             //remove selected category
-            var removeSelected = new List<PostCategory>();
-            foreach(var postCategories in context.PostCategories){
-                if(!SelectedCategoryIds.Contains(postCategories.CategoryId)){
-                removeSelected.Add(postCategories);
+            var removeCategories = new List<PostCategory>();
+            foreach (var postCategories in model.PostCategories)
+            {
+                if (!SelectedCategoryIds.Contains(postCategories.CategoryId))
+                {
+                    removeCategories.Add(postCategories);
                 }
             }
-            foreach(var postCategory in removeSelected){
+            //remove oldselect
+            foreach (var postCategory in removeCategories)
+            {
                 context.PostCategories.Remove(postCategory);
             }
             //add new selected Category
-            foreach(var item in SelectedCategoryIds.Where(item=>!context.PostCategories.Any(pc=>pc.CategoryId==item))){
-                context.PostCategories.Add(new PostCategory{CategoryId=item,PostId=post.Id});
+            foreach (var item in SelectedCategoryIds)
+            {
+                if (!model.PostCategories.Any(pc => pc.CategoryId == item))
+                    model.PostCategories.Add(new PostCategory { CategoryId = item, PostId = post.Id });
             }
-            context.Update(post);
+            model.Title = post.Title;
+            context.SaveChanges();
             return RedirectToAction(nameof(Index));
         }
-
-        [HttpPost]
         public IActionResult Delete(int id)
         {
             var post = context.posts.Find(id);
-            context.posts.Remove(post);
+            return View(post);
+        }
+        [HttpPost]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var post = context.posts.Find(id);
+            if (post == null)
+                return RedirectToAction(nameof(Index));
+            context.Remove(post);
             context.SaveChanges();
             return RedirectToAction(nameof(Index));
         }
